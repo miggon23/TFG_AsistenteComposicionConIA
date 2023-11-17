b07be7725ec13784b179916eecb3def6ac8e7d7a
Vicnuel5 <81177633+Vicnuel5@users.noreply.github.com>
2023-11-17 21:01:37 +0100

    harmonyGenerator

--- Harmonizer/harmonyGenerator.py
+++ Harmonizer/harmonyGenerator.py
@@ -1,16 +1,22 @@
 import MidiUtils
 import Song
-import Note
+import Scale
 
 class HarmonyGenerator:
     def generate(input):
-        melody_ = MidiUtils.read_midi_song(input)
-        song_ = Song.Song(melody_[:-1], Note.Note("A"))
-        song_.choose_scale()
-        harmony_ = song_.armonize(ticksPerSlice=1.0)
-        song_.print_chord_analysis()
 
-        output = "midi/output_harmony.mid"
-        MidiUtils.write_midi_song(output, harmony_)
+        someChords = {
+            "": Scale.Scale("1 3 5"),  # Mayor
+            "-": Scale.Scale("1 b3 5"),  # Menor
+            "-b5": Scale.Scale("1 b3 b5"),  # Disminuida
+            "7": Scale.Scale("1 3 5 b7"),  # Dominante 
+        }
 
-        return output
+        melody = MidiUtils.read_midi_song("midi/input_song.mid")
+        song = Song.Song(melody)
+        song.choose_scale()
+        harmony = song.armonize(ticksPerSlice = 4.0, possibleChords = someChords)
+        harmonyMidi = "midi/output_harmony.mid"
+        MidiUtils.write_midi_song(harmonyMidi, harmony)
+
+        return harmonyMidi
