cf0399e94d2aeac189086fc826df6e859b407f78
rodsan05 <rodsan05@ucm.es>
2023-11-24 21:31:17 +0100

    faltaba un return

--- Basslines/basslineGenerator.py
+++ Basslines/basslineGenerator.py
@@ -3,6 +3,6 @@ from basslinePattern import BasslinePattern
 
 class BasslineGenerator:
     def generate():
-        line = BasslinePattern.generatePatterns()
+        return BasslinePattern.generatePatterns()
 
         # midiUtils.write_midi_song("midi/output_"+style.name+"_drumPatternA.mid", drumPatternA)
