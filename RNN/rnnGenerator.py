fff3662d586d6259b72913a2be0d107b004d8468
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2024-02-10 13:14:42 +0100

    rnn 17 accuracy

--- RNN/rnnGenerator.py
+++ RNN/rnnGenerator.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 import tensorflow as tf
 from tensorflow import keras
@@ -14,6 +15,18 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import confusion_matrix
+
+def generate_sequences(X, y, seq_length):
+    input_sequences = []
+    output_sequences = []
+
+    for i in range(len(X) - seq_length):
+        seq_in = X[i:i + seq_length, :]
+        seq_out = y[i + seq_length, :]
+        input_sequences.append(seq_in)
+        output_sequences.append(seq_out)
+
+    return np.array(input_sequences), np.array(output_sequences)
 
 def train_rnn():
     path = "Datasets/Cleaned/"
@@ -38,21 +51,29 @@ def train_rnn():
     # y = df['next_note']
 
     # Aplicar one-hot encoding a las columnas 'curr_note' y 'next_note'
-    encoder = LabelEncoder()
-    X = df['curr_note']
+    X = df.drop(['next_note', 'curr_note'], axis=1)
     y = df['next_note']
+
+    scaling = StandardScaler()
+    scaling.fit(X)
+    X = scaling.transform(X)
+    # X = X.values
 
     label_encoder = LabelEncoder()
     label_encoder.fit(keys)
-    X_encoded = label_encoder.transform(X)
+    # X_encoded = label_encoder.transform(X)
     y_encoded = label_encoder.transform(y)
 
     # Convertir las notas codificadas a one-hot encoding
-    X_onehot = to_categorical(X_encoded)
+    # X_onehot = to_categorical(X_encoded)
     y_onehot = to_categorical(y_encoded)
 
+    # Generamos secuencias de notas
+    seq_length = 5  # Ajusta según la longitud de la secuencia que desees
+    input_data, output_data = generate_sequences(X, y_onehot, seq_length)
+
     # Split the data into training and test sets
-    x_train, x_val, y_train, y_val = train_test_split(X_onehot, y_onehot, test_size=0.2)
+    x_train, x_val, y_train, y_val = train_test_split(input_data, output_data, test_size=0.2)
 
     early_stopping = EarlyStopping(
         monitor='val_loss',  # Métrica a monitorear (puede ser 'val_accuracy', 'val_loss', etc.)
@@ -62,10 +83,8 @@ def train_rnn():
 
     model = keras.Sequential()
 
-    model.add(layers.Dense(64, activation='relu', input_shape=(n_out,)))
-
-    # Agrega una capa de Reshape para agregar la dimensión de los pasos de tiempo
-    model.add(layers.Reshape((1, 64)))
+    # Capa de entrada
+    model.add(layers.Input(shape=(seq_length, 2)))
 
     # Capa LSTM
     model.add(layers.LSTM(64))
@@ -84,7 +103,7 @@ def train_rnn():
     history = model.fit(
         x_train,
         y_train,
-        batch_size=32,
+        batch_size=256,
         epochs=5000,
         validation_data=(x_val, y_val),
         callbacks=[early_stopping]
