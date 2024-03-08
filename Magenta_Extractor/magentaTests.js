b406a3bb9a59202a51622517fc42a732436f3592
rodsan05 <80984128+rodsan05@users.noreply.github.com>
2024-03-09 00:18:13 +0100

    ejecutar el codigo de magenta en js desde python
    de momento no escribe midis, pero recibe un json en python

--- Magenta_Extractor/magentaTests.js
+++ Magenta_Extractor/magentaTests.js
@@ -1,16 +1,30 @@
-const { core, midi, sequenceProtoToMidi, midiToSequenceProto } = require('@magenta/music/node/core');
+// Este script genera una noteSeq con magenta en JS y lo escribe por la salida estandar, esta salida estandar 
+// sera recogida por python con subprocess
+
 const mm = require('@magenta/music/node/music_rnn');
 const fs = require('fs');
 
-// Initialize the model.
+// Guardar la referencia de la salida estándar original
+const originalStdoutWrite = process.stdout.write;
+
+// Suprimir la salida estándar
+// Esto es necesario para evitar el mensaje de inicializacion de magenta en la salida estandar
+process.stdout.write = function() {};
+
+// Restaura la función write original después de la inicialización del modelo
 music_rnn = new mm.MusicRNN('https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/basic_rnn');
-music_rnn.initialize();
+music_rnn.initialize().then(() => {
+    process.stdout.write = originalStdoutWrite;
+    // generamos la secuencia de notas
+    generate();
+});
 
 rnn_steps = 20;
 rnn_temperature = 1.5;
 
 function generate() {
 
+    // melodia inicial para que el modelo la continue, por el momento es un Do y ya
     INI_MEL = {
         notes: [
             { pitch: 60, quantizedStartStep: 0, quantizedEndStep: 1 }
@@ -23,20 +37,11 @@ function generate() {
     music_rnn
         .continueSequence(INI_MEL, rnn_steps, rnn_temperature)
         .then(samples => {
-
-            //Conversión a MIDI
-            const midiFile = sequenceProtoToMidi(samples);
-            const midiBuffer = Buffer.from(midiFile);;
-            const midiFileName = 'midi\\magenta_melody.mid';
-
-            fs.writeFile(midiFileName, midiBuffer, 'binary', err => {
-                if (err) {
-                    console.error('Error al guardar el archivo MIDI:', err);
-                } else {
-                    console.log(`Archivo MIDI guardado como ${midiFileName}`);
-                }
-            });
+            // Convertir NoteSequence a JSON y enviarlo a la salida estándar
+            const jsonSequence = JSON.stringify(samples);
+            process.stdout.write(jsonSequence);
         });
 }
 
-generate()
+// Llamar a la función generate() para generar la secuencia de notas
+// generate();
