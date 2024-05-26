0e4ffd79ec124f588361658fc54d34db20063c5b
Miguel González Pérez <miggon23@ucm.es>
2024-05-27 00:54:35 +0200

    Update install_requirements.bat

--- install_requirements.bat
+++ install_requirements.bat
@@ -3,10 +3,11 @@
 if not exist env (
     py -3.9 -m venv env
 
-    cd MagentaGenerator
-    call install_magenta_dependencies.bat
-    cd ..
 )
+
+cd MagentaGenerator
+call install_magenta_dependencies.bat
+cd ..
 
 call ./env/Scripts/activate
 
